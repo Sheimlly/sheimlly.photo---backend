76d6276c3eb262cc7e0dcb869d222568d174b544
Emilia <sheimlly.dev@gmail.com>
2023-11-26 17:19:07 +0100

    requirements

--- requirements.py
+++ requirements.py
@@ -2,7 +2,7 @@ django==4.2
 django-cors-headers==4.3
 djangorestframework==3.14
 djangorestframework-simplejwt==5.3
-python_dotenv==1.0
+python_dotenv>=1.0
 psycopg2==3.1
 Pillow==10.1
 django-filter==23.3
